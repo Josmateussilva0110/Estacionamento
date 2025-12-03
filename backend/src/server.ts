b6f71658a441a8532eab8bc23b2aea830fbc3138
Jose Mateus <jmateussilva044@gmail.com>
2025-12-03 10:46:48 -0300

    feat: sessões

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,48 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import session from "express-session"
+import PgSession from "connect-pg-simple"
+import { Pool } from "pg"
 
 dotenv.config()
 
 const app = express()
 
+
+const pgPool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+  ssl: false
+})
+
+
+
+const PostgresSession = PgSession(session)
+
 app.use(cors())
 app.use(express.json())
+
+app.use(
+  session({
+    store: new PostgresSession({
+      pool: pgPool,
+      tableName: "session",
+      createTableIfMissing: true,
+    }),
+    secret: process.env.SECRET || "fallback_secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, 
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 2, // 2 horas
+    },
+  })
+)
 
 app.get("/", (request, response) => {
   response.json({ status: "API rodando com TypeScript 🚀" })
@@ -18,4 +53,3 @@ const PORT = 8080
 app.listen(PORT, () => {
   console.log(`🔥 Servidor rodando na porta ${PORT}`)
 })
-
