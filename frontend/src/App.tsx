c925580e18a4b6f4b419f3a2f91493cef1d3ca73
Jose Mateus <jmateussilva044@gmail.com>
2025-12-12 08:39:22 -0300

    feat: footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import AppRoutes from "./routes/Index"
 import FlashMessage from "./components/ui/Message"
 import NavBar from "./components/layout/NavBar"
+import Footer from "./components/layout/Footer"
 
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <NavBar />
       <FlashMessage />
-      <AppRoutes />
-    </>
+
+      {/* Conteúdo cresce e empurra a footer */}
+      <div className="flex-1">
+        <AppRoutes />
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
