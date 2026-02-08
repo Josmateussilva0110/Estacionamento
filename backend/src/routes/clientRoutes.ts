8e9e1df1fdaf982aaec8b6bbccbc06835f9192ed
Mateus <jmateussilva044@gmail.com>
2026-02-08 13:14:18 -0300

    feat: routes authenticated

--- backend/src/routes/clientRoutes.ts
+++ backend/src/routes/clientRoutes.ts
@@ -4,18 +4,19 @@ import { RegisterClientSchema } from "../schemas/clientSchema"
 import { UserIdParamSchema } from "../schemas/userIdSchema"
 import { IdParamSchema } from "../schemas/IdParamSchema"
 import { validate } from "../middleware/validate"
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated"
 
 
 const router = Router()
 
 
-router.post("/client/register", validate(RegisterClientSchema), ClientController.register)
-router.get("/clients/:user_id", validate(UserIdParamSchema, "params"), ClientController.getClients)
-router.get("/clients/vehicle/:user_id", validate(UserIdParamSchema, "params"), ClientController.getClientAndVehicle)
-router.get("/clients/pagination/:user_id", validate(UserIdParamSchema, "params"), ClientController.listClients)
-router.get("/client/:id", validate(IdParamSchema, "params"), ClientController.getById)
-router.delete("/client/:id", validate(IdParamSchema, "params"), ClientController.remove)
-router.put("/client/:id", validate(RegisterClientSchema), validate(IdParamSchema, "params"), ClientController.edit)
+router.post("/client/register", validate(RegisterClientSchema), ensureAuthenticated, ClientController.register)
+router.get("/clients/:user_id", validate(UserIdParamSchema, "params"), ensureAuthenticated, ClientController.getClients)
+router.get("/clients/vehicle/:user_id", validate(UserIdParamSchema, "params"), ensureAuthenticated, ClientController.getClientAndVehicle)
+router.get("/clients/pagination/:user_id", validate(UserIdParamSchema, "params"), ensureAuthenticated, ClientController.listClients)
+router.get("/client/:id", validate(IdParamSchema, "params"), ensureAuthenticated, ClientController.getById)
+router.delete("/client/:id", validate(IdParamSchema, "params"), ensureAuthenticated, ClientController.remove)
+router.put("/client/:id", validate(RegisterClientSchema), validate(IdParamSchema, "params"), ensureAuthenticated, ClientController.edit)
 
 
 
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ import { validate } from "../middleware/validate"
 import { RegisterSchema } from "../schemas/registerSchema"
 import { LoginSchema } from "../schemas/loginSchema"
 import { IdParamSchema } from "../schemas/IdParamSchema"
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated"
 
 
 const router = Router()
@@ -13,6 +14,6 @@ router.post("/register", validate(RegisterSchema), UserController.register)
 router.post("/login", validate(LoginSchema), UserController.login)
 router.get("/user/session", UserController.session)
 router.post("/user/logout", UserController.logout)
-router.get("/user/:id", validate(IdParamSchema, "params"), UserController.getById)
+router.get("/user/:id", validate(IdParamSchema, "params"), ensureAuthenticated, UserController.getById)
 
 export default router
